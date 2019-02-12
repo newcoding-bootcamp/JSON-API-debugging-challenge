1db67cfe6513b3372c70ec053a8fc22b5b47f657
Ian Nicholson <ipnicholson@gmail.com>
2019-02-12 12:43:09 -0800

    bug 2

--- routes/todos.js
+++ routes/todos.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models'); // index file in /models will direct require() to the right model
-const helpers = require('../helpers/todos')
+const helpers = require('./helpers/todos')
 
 router.route('/')
   .get(helpers.getTodos)
